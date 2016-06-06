3e9d3db50ed687dae9012af5fcfe8e18553d39f1
Raif Harik <reharik@gmail.com>
2016-06-05 21:49:17 -0500

    still working on the css

--- src/js/components/Week.js
+++ src/js/components/Week.js
@@ -6,7 +6,7 @@ export default ({week, tasks, actions, config}) => {
 	var timeStrings = getHalfHoursForDay(config);
 
 	var dayByTimeElements = (week, time, index) => week.map((day, index) =>
-		(<div key={index} onClick={(e) => e.target === e.currentTarget ? actions.selectSlot(day.format('M/D/YYYY'), time):''}>
+		(<div key={index} className="week-slot" onClick={(e) => e.target === e.currentTarget ? actions.selectSlot(day.format('M/D/YYYY'), time):''}>
 			<Tasks tasks={ tasksPerTime(day, time) } actions={actions} view="week" />
 		</div>));
 
@@ -16,23 +16,23 @@ export default ({week, tasks, actions, config}) => {
 		(<div className="week-display">
 			{timeStrings.map((time, index) =>
 				<div key={index} className="time-row">
-						<div className="time">{time}</div>
+						<div className="time"><span>{time}</span></div>
 						{ dayByTimeElements(week, time, index)}
 				</div>)}
 		</div>);
 
 	return (<div className="week-view">
 			<div className="week days-header mdl-layout__header-row mdl-shadow--1dp">
-				<ul>
-					<li className="time-zone">CDT</li>
-					<li>Sunday</li>
-					<li>Monday</li>
-					<li>Tuesday</li>
-					<li>Wednesday</li>
-					<li>Thursday</li>
-					<li>Friday</li>
-					<li>Saturday</li>
-				</ul>
+				<div>
+					<div className="time-zone">CDT</div>
+					<div>Sunday</div>
+					<div>Monday</div>
+					<div>Tuesday</div>
+					<div>Wednesday</div>
+					<div>Thursday</div>
+					<div>Friday</div>
+					<div>Saturday</div>
+				</div>
 			</div>
 			{weekJSX}
 		</div>
