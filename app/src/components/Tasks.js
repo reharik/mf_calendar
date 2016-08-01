0a9a21521bb629b925df76188eb2d3d6e3dd7247
Raif Harik <reharik@gmail.com>
2016-08-01 17:55:37 -0500

    some minor tweaks

--- app/src/components/Tasks.js
+++ app/src/components/Tasks.js
@@ -2,21 +2,28 @@ import React, { PropTypes } from 'react';
 import Color from 'color';
 
 const Tasks = ({tasks, config, dispatch}) => {
+  const styleTaskItem = (t, index) => ({width: t.width + '%',
+    height: 18 * t.slots + 'px',
+    marginLeft: index > 0 || t.margin === 0 ? '0' : t.margin + 1 + '%',
+    zIndex: t.column,
+    border: '2px solid ' + Color(t.color).darken(0.3).hexString()  //eslint-disable-line new-cap
+  });
+
+  const styleTaskItemTitle = t =>
+    ({ backgroundColor: Color(t.color).darken(0.2).hexString() }); //eslint-disable-line new-cap
+
   const selectTaskAction = task => config.taskClickedAction(task.id, task, dispatch);
 
-  /* eslint-disable new-cap */
   return (<div className="task">
 		{ tasks.sort((a, b) => a.margin > b.margin).map((t, index)=> (<div className="task__item" key={index}
-  style={{width: t.width + '%', height: 18 * t.slots + 'px',
-	marginLeft: index > 0 || t.margin === 0 ? '0' : t.margin + 1 + '%', zIndex: t.column }}
+  style={styleTaskItem(t, index)}
   onClick={() => selectTaskAction(t)}>
-  <div className="task__item__title" style={{ backgroundColor: Color(t.color).darken(0.2).hexString()}} >{t.title}</div>
+  <div className="task__item__title" style={styleTaskItemTitle(t)} >{t.title}</div>
   <div className="task__item__display" style={{ backgroundColor: t.color}}> {t.display}</div>
 </div>)
 		)}
   </div>);
 };
-/* eslint-enable new-cap */
 
 Tasks.propTypes = {
   tasks: PropTypes.array.isRequired,
